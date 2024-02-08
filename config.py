a5b16d56730d517112aed1b79eb44fd79940ff2b
smorenolasa1 <smoreno.ieu2021@student.ie.edu>
2024-02-08 15:31:51 +0100

    removed secret openaikey

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ vonage_sandbox_number = "14157386102"  # Replace with your actual Vonage sandbox
 vonage_authorization_header = "Basic NmZjOWQ4MmQ6WkE1VnRWSEJYU2VmcDNhWg=="  # The base64-encoded credentials
 
 # OpenAI accounts
-openai_key = "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"  # Your OpenAI API Key
+openai_key = ""  # Your OpenAI API Key
